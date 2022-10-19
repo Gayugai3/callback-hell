97f0b4376e15ee851255fd76bc4cbcc6ec8fc124
Gayugai3 <112932746+Gayugai3@users.noreply.github.com>
2022-10-19 13:38:04 +0530

    Call back hell - countdown
    using setTimeout()

---
+++ script.js
@@ -0,0 +1,30 @@
+var count = document.getElementById("no");
+var str = document.getElementById("str");
+
+setTimeout(()=>{count.innerHTML=10
+    setTimeout(()=>{count.innerHTML=9
+        setTimeout(()=>{count.innerHTML=8
+            setTimeout(()=>{count.innerHTML=7
+                setTimeout(()=>{count.innerHTML=6
+                    setTimeout(()=>{count.innerHTML=5
+                        setTimeout(()=>{count.innerHTML=4
+                            setTimeout(()=>{count.innerHTML=3
+                                setTimeout(()=>{count.innerHTML=2
+                                    setTimeout(()=>{count.innerHTML=1
+                                       
+                                            setTimeout(()=>{
+                                                count.innerHTML="";
+                                                str.innerHTML="Happy Independence Day";
+                    
+                                            },1000)
+                                      
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000);
